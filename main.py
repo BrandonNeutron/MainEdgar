f9198f300a22d89b5d3275c8116242dab07fee1f
Brandon Hahn <blh16b@my.fsu.edu>
2019-07-12 22:16:29 -0400

    Successfully parses four paramaters

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import sys
+
+def main():
+    tickers = []
+    action = ""
+    fromDate = ""
+    toDate = ""
+
+    if len(sys.argv) > 1:
+        for arg in sys.argv:
+            if "ticker" in arg:
+                tickers = parseCSVs(arg)
+            if "action" in arg:
+                action = arg[arg.find("=") + 1:]
+            if "from" in arg:
+                fromDate = arg[arg.find("="):]
+            if "toDate" in arg:
+                toDate = arg[arg.find("="):]
+    print("ACTION: ", action)
+    print("TICKERS: ", tickers)
+    print("FROM: ", fromDate)
+    print("TO: ", toDate)
+
+
+# tickers=GOOG,F,CGC
+def parseCSVs(arg):
+    return arg[arg.find("=") + 1:].split(',')
+
+main()
